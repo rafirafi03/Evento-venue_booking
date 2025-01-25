5bc0fd04c57cd8587ecc548cd650cb5dd700c37f
rafirafi03 <ahamedrafirafi03@gmail.com>
2025-01-25 15:19:18 +0530

    updated

--- frontend/src/app/(components)/(user)/login/page.tsx
+++ frontend/src/app/(components)/(user)/login/page.tsx
@@ -69,7 +69,7 @@ const Page = () => {
       toast.dismiss(loadingToast)
 
       if (res.success) {
-        toast.success(<b>{res.message}!</b>)
+        toast.success(<b>{res.message}.,.,</b>)
         const token = res.token;
         if (typeof window !== "undefined") {
         localStorage.setItem('authUserToken', token)
