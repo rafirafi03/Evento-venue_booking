3b7eafcf37d9e08b2379e457c7e332dd4d05fa76
rafirafi03 <ahamedrafirafi03@gmail.com>
2025-01-28 13:43:15 +0530

    updated

--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -3,4 +3,7 @@ import { io } from "socket.io-client";
 export const socket = io("wss://api.eventobooking.site", {
   transports: ["websocket"],
   autoConnect: false,
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
 });
