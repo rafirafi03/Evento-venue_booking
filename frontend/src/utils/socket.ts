a4f27bcd0cf14b41d036f769a82215dfe5675e81
rafirafi03 <ahamedrafirafi03@gmail.com>
2025-01-28 12:35:20 +0530

    updated. added socket.io in io url

--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("wss://api.eventobooking.site", {
+export const socket = io("wss://api.eventobooking.site/socket.io", {
   transports: ["websocket"],
   autoConnect: false,
 });
