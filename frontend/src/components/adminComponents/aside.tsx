c428a9dc796dac1fbeb7955be190c5fe9b419288
rafirafi03 <ahamedrafirafi03@gmail.com>
2025-02-04 19:42:58 +0530

    updated

--- frontend/src/components/adminComponents/aside.tsx
+++ frontend/src/components/adminComponents/aside.tsx
@@ -77,7 +77,7 @@ export default function Page() {
 
             <li
               className="cursor-pointer"
-              onClick={() => router.push("/admin/userslist")}
+              onClick={() => router.push("/admin/usersList")}
             >
               <a
                 className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-black font-extrabold hover:bg-gray-100 dark:hover:bg-red-200 group ${
