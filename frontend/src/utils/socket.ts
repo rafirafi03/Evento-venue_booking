c4306ba2dde25c2e23534d0904f5b948949d6868
rafirafi03 <ahamedrafirafi03@gmail.com>
2025-02-04 10:05:24 +0530

    updated

--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -1,7 +1,5 @@
 import { io } from "socket.io-client";
 
 export const socket = io("wss://api.eventobooking.site", {
-  path: "/socket.io",
-  withCredentials: true,
   transports: ["websocket", "polling"],
 });
