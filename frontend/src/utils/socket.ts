d4c925c1be689b2125a24533d53f848aabea7d1a
rafirafi03 <ahamedrafirafi03@gmail.com>
2025-02-04 10:29:44 +0530

    deployed

--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
-export const socket = io("wss://api.eventobooking.site/chat", {
-  path: "/socket.io",
+export const socket = io("wss://api.eventobooking.site", {
+  path: "/chat",
   withCredentials: true,
   transports: ["websocket", "polling"],
 });
